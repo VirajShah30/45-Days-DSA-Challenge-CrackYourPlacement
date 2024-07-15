e86cebe5bebb515a65db96a991bbe54a2a9cfbee
Viraj Shah <virajshah.vs30@gmail.com>
2024-07-14 20:38:20 -0500

    Solution for Find the Duplicate Number in Java

---
+++ Find the Duplicate Number/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int n = arr.length;
+        int[] freq = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            if (freq[arr[i]] == 0) {
+                freq[arr[i]]++;
+            } else {
+                return arr[i];
+            }
+        }
+        return 0;  
+    }
+}
